5ae23fdbdb3476034334f053a5093f9c91577905
Adam Freisinger <freegyes@gmail.com>
2023-04-01 10:28:00 +0200

    add penplot.js env

---
+++ projects/plots-for-penplotjs/hello.js
@@ -0,0 +1,23 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  return (props) => {
+    // Destructure what we need from props
+    const { context, width, height } = props;
+
+    // Fill the canvas with pink
+    context.fillStyle = 'pink';
+    context.fillRect(0, 0, width, height);
+
+    // Now draw a white rectangle in the center
+    context.strokeStyle = 'white';
+    context.lineWidth = 4;
+    context.strokeRect(width / 4, height / 4, width / 2, height / 2);
+  };
+};
+
+canvasSketch(sketch, settings);
