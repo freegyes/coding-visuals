7925df06c75d8adc6d499b0c449dda8fe38dc7fe
Adam Freisinger <freegyes@gmail.com>
2023-04-01 10:27:36 +0200

    update p5 env

--- projects/hello-p5/sketch.js
+++ projects/hello-p5/sketch.js
@@ -1,15 +1,7 @@
-let canvasSize = 400;
-let iterations = 256;
-
 function setup() {
-  createCanvas(canvasSize, canvasSize);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(256);
-  let c = color(128, 10);
-  for (let i = 0; i < iterations; i++) {
-    ellipse(canvasSize / 2, canvasSize/2, noise(i) * canvasSize, noise(i) * canvasSize);
-    fill(c);
-  }
+  background(220);
 }
